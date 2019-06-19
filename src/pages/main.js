11050ff1bfcca3319f9b18ba0cc4c94a9aac078c
João Guilherme Barbosa <joaogbarbosa@gmail.com>
2019-06-19 11:01:56 -0300

    pokeapi.co inicial integration

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
-
+import api from "../services/api";
 export default class Main extends Component {
+  componentDidMount() {
+    this.loadPokemons();
+  }
+  state = {
+    pokemons: []
+  };
+  loadPokemons = async () => {
+    const response = await api.get("/pokemon");
+
+    const { data } = response;
+    const { results } = data;
+    console.log("Pokemon", results);
+
+    this.setState({ pokemons: results });
+  };
   render() {
     return (
       <View>
-        <Text>Main</Text>
+        <Text>Pokenons</Text>
+        {this.state.pokemons.map(pokemon => (
+          <Text key={pokemon.name}>{pokemon.name}</Text>
+        ))}
       </View>
     );
   }
